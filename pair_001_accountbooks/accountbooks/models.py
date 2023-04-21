fdafdbda99419af60c794adbfde00082e1cef0ed
Shureeshu <ruby04.s.lee@gmail.com>
2023-04-22 00:35:26 +0900

    [ADD] model AccountBook

--- pair_001_accountbooks/accountbooks/models.py
+++ pair_001_accountbooks/accountbooks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class AccountBook(models.Model):
+    note = models.CharField(max_length=80) # 내용	
+    description = models.TextField(null=True) #	메모
+    category = models.CharField(max_length=20) # 분류
+    amount = models.IntegerField(default=0) # 사용 금액
+    date = models.DateField() # 사용 날짜	
+    created_at = models.DateTimeField(auto_now_add=True) # 생성 날짜
+    updated_at = models.DateTimeField(auto_now=True) # 수정 날짜
