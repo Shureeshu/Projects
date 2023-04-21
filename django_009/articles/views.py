61904bd7afde9bc43ea704f1f36c42dfbdf12c6e
Shureeshu <ruby04.s.lee@gmail.com>
2023-04-22 00:21:19 +0900

    페이지네이터

--- django_009/articles/views.py
+++ django_009/articles/views.py
@@ -2,12 +2,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
+from django.core.paginator import Paginator
 
 
 def index(request):
     articles = Article.objects.order_by('-pk')
+    """
+        [int] page
+        현재 주소의 페이지 번호를 할당받는 변수
+        주소에 page(키)에 대한 값이 없으면 1 할당
+    """
+    page = request.GET.get('page', '1')
+
+    """
+        [int] per_page
+        페이지를 나누는 기준
+        e.g. 10 -> 데이터를 10개를 기준으로 나눔.
+    """
+    per_page = 1
+
+    """
+        [Paginator 인스턴스] paginator
+        첫 번째 인자 : 페이지네이션을 적용할 데이터(queryset)
+        두 번째 인자 : per_page
+    """
+    paginator = Paginator(articles, per_page)
+
+    """
+        [Page 인스턴스] page_obj
+        출력할 데이터 및 페이지네이션을 구현을 위한 데이터가 저장된 변수
+        반복문으로 순회하면 페이징 처리가 된 데이터가 요소가 됨.
+    """
+    page_obj = paginator.get_page(page)
+
     context = {
-        'articles': articles,
+        'articles': page_obj,
+        'last_page': paginator.num_pages,
     }
     return render(request, 'articles/index.html', context)
 
